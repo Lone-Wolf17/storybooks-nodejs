ab119e7a2e99ec97390654837e93f177c77015fd
lonewolf <adeyanju.akorede1@gmail.com>
2021-08-09 17:10:21 +0100

    ** Protect Routes with Auth Middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth.js');
 
 // @desc         Login/Landing Page
 // @route       Get /
-router.get('/', (req, res) => {
+// Middleware ensures that signed in User can't access this route
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login'
     });
@@ -11,7 +13,8 @@ router.get('/', (req, res) => {
 
 // @desc         Dashbaord
 // @route       Get /dashboard
-router.get('/dashboard', (req, res) => {
+// Middleware ensures that only signed in user can access this route
+router.get('/dashboard', ensureAuth, (req, res) => {
     res.render('dashboard');
 });
 
